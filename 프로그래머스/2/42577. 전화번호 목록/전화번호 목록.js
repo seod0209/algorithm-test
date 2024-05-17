710a3aa185fbf99f64af40fd713c71dec25ddea1
Dongee Seo <74766722+seod0209@users.noreply.github.com>
2024-05-17 19:19:06 +0900

    [level 2] Title: 전화번호 목록, Time: 127.95 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,58 +1,10 @@
-// 기존 답안
-// function solution(phone_book) {
-//     var answer = true;
-//  	phone_book.sort()
-//     const map = new Map()
-    
-//     for(let i = 0; i<phone_book.length; i++){
-//        for(let j = i; j<phone_book.length; j++){
-//          const a = (phone_book[i])
-//          const b = (phone_book[j])
-        
-//          if(a !== b){
-//        		if(b.includes(a)) return false
-//    		 }
-//     }
-//     return answer;
-// }
-
-// 참고 답안
 function solution(phone_book) {
- var answer = true;
-  	const hashMap = {}
-    
-    // hashMap의 key를 전화번호로 저장.
-    phone_book.forEach( num => {
-      hashMap[num] = true
-    })
-  
-  	for(const number of phone_book){
-      if(!answer) break;
-      
-      let prefix = "";
-      
-      // 현재 검사 중인 전화번호(number)의 각 자릿수(digit)에 대해 반복
-      for(const digit of number){
-        
-        // 1. 접두사 생성.
-        // 현재까지의 접두사에 현재 자릿수를 더한다.
-        // 이렇게 하면 접두사가 하나씩 늘어난다.
-        // 접두사의 길이에 따라서 점차적으로 전체 전화번호의 자릿수를 확인가능
-        prefix += digit
- 
-        // 2. 현재 전화번호가 다른 전화번호의 접두사인지를 확인
-        
-        // hashMap[prefix]
-        // 현재까지 만들어진 접두사가 해시맵에 존재하는지 확인
-        // 만약 존재한다면, 해당 접두사가 이미 다른 전화번호의 접두사임을 의미
-        
-        // prefix !== number
-        // 현재까지 만들어진 접두사가 현재 검사 중인 전화번호(number)와 같지 않은지 확인
-        // 만약 같다면, 이는 자기 자신의 전화번호를 검사하는 것이므로 접두사가 아님.
-        if(hashMap[prefix] && prefix !== number){
-          answer = false
-          break;
-        }
+    var answer = true;
+    phone_book.sort()
+   
+    for(let i = 0; i < phone_book.length -1; i++){
+      if(phone_book[i+1].startsWith(phone_book[i])){
+        answer = false
       }
     }
     return answer;
