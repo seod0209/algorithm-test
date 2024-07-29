9f0d058eb0136869e61b8e462116bdb29c9406d4
Dongee Seo <74766722+seod0209@users.noreply.github.com>
2024-07-29 10:30:05 +0900

    [Bronze V] Title: 구구단, Time: 124 ms, Memory: 9368 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.ts
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const inputData = fs.readFileSync('/dev/stdin').toString();
+const num = Number(inputData);
+
+for(let i = 1; i < 10; i++){
+	console.log(num, "*", i, "=", num*i); 
+  // console.log(num + " * " + i + " = " + num*i);
+}
